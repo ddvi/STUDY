415be5666d4e08a8d54050607344af5af9cc882b
ddvi <lgr19710903@gmail.com>
2020-11-05 13:01:41 +0800

    20201105

---
+++ Python/脚本/demo2.py
@@ -0,0 +1,15 @@
+account = 'lzx'
+password = '123456'
+print('please input account')
+user_account = input()    
+print('please input password')
+user_password = input()
+if user_account == account and user_password == password :
+    print('sucess')
+else:
+    print('fail')
+
+if a:
+    pass
+else:
+    pass
