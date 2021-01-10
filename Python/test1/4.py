c226fa5717c8c870a889a46b200333d225d351a0
ddvi <lgr19710903@gmail.com>
2021-01-10 19:23:02 +0800

    0110

--- Python/test1/4.py
+++ Python/test1/4.py
@@ -1,3 +1,11 @@
-if __name__ == '__main__':
-    print('This is app')
-print('This is a module') 
+# coding:utf-8
+from evdev import InputDevice, ecodes, list_devices, categorize
+import signal, sys
+import evdev
+import redis
+
+
+global dev
+devices = map(InputDevice, list_devices())
+for device in devices:
+        print(device.name)
--- Python/test1/c6.py
+++ Python/test1/c6.py
@@ -1,7 +1,28 @@
 class Student():
-    name = ''
-    age = 0
-    
-    def print_file(self):
-        print('name:' + self.name)
-        print('age:' + str(self.age))
+    # 类变量
+    su = 0
+    score = 0
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.__class__.su += 1
+
+    def marking(self, score):
+        self.score = score
+        print(self.name + '同学本次考试分数为：' + str(self.score))
+    def do_english_homework():
+        print("englishhomework")
+    def do_homework(self):
+        self.do_english_homework(self)
+        print('homework')
+    @classmethod
+    def plus_su(cls):
+        cls.su += 1
+        print("当前班级总人数为： " + str(cls.su))
+    @staticmethod
+    def add(x, y):
+        print(Student.su)
+        print('this is a static method')
+student1=Student('a', 1)
+student1.marking(59)
